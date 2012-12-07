496beda1ba6b0cf08ade6be14e17d516b45ecae2
lili <lilizing@qq.com>
2012-12-07 17:05:00 +0800

    Revert "Revert "xcode里面提交的内容""
    This reverts commit 44abbc83ff86dd799f3f8082eda277f1fea7c287.

---
+++ demo/demo/BIDAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  BIDAppDelegate.h
+//  demo
+//
+//  Created by lili on 12-12-7.
+//  Copyright (c) 2012年 tq365. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class BIDViewController;
+
+@interface BIDAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) BIDViewController *viewController;
+
+@end
