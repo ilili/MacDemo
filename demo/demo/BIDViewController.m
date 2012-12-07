d2c1ab2574c009be47b36cbd3befbc6f717d7deb
lili <lilizing@qq.com>
2012-12-07 17:00:02 +0800

    xcode里面提交的内容

---
+++ demo/demo/BIDViewController.m
@@ -0,0 +1,29 @@
+//
+//  BIDViewController.m
+//  demo
+//
+//  Created by lili on 12-12-7.
+//  Copyright (c) 2012年 tq365. All rights reserved.
+//
+
+#import "BIDViewController.h"
+
+@interface BIDViewController ()
+
+@end
+
+@implementation BIDViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
