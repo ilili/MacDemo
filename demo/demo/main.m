fbc93287f3201a704a95b9203791a51a5f0559e2
lili <lilizing@qq.com>
2012-12-07 17:02:16 +0800

    客户端提交
    客户端提交内容

---
+++ demo/demo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  demo
+//
+//  Created by lili on 12-12-7.
+//  Copyright (c) 2012年 tq365. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "BIDAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([BIDAppDelegate class]));
+    }
+}
